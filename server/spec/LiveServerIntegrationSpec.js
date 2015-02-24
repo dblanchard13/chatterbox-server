85d1feb65a68d6328933ab2b8e1995437b4826ae
dblanchard13 <davidb.prof@gmail.com>
2015-02-24 10:43:50 -0800

    Code cleanup

--- server/spec/LiveServerIntegrationSpec.js
+++ server/spec/LiveServerIntegrationSpec.js
@@ -43,7 +43,6 @@ describe('server', function() {
     };
 
     request(requestParams, function(error, response, body) {
-      console.log(response.statusCode);
       expect(response.statusCode).to.equal(201);
       done();
     });
